0af424a5f8aa5ffcae9e68b0b9041e4fc3d5daee
Andres Arpi <aarpi@itba.edu.ar>
2017-07-25 18:53:22 +0000

    remodify the rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,9 @@ require 'rspec/rails'
 # require database cleaner at the top level
 require 'database_cleaner'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+
 # [...]
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Shoulda::Matchers.configure do |config|
@@ -23,6 +26,8 @@ RSpec.configuration do |config|
   # [...]
   # add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
+
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
