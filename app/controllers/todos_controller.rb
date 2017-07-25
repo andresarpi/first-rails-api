98f9e25b3cfa0e97fc9fad1fe6bc35e5aff3655e
Andres Arpi <aarpi@itba.edu.ar>
2017-07-25 19:00:57 +0000

    Start with the todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,16 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+  
+  # GET /todos
+  def index
+    @todos = Todo.all
+    json_response(@todos)
+  end
+
+  private
+    
+    def set_todo
+      @todo = Todo.find([params[:id]])
+    end
+  
 end
