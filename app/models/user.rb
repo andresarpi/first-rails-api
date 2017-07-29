6ad5c17f4f7c1ef54a1587f6449300599613939f
Andres Arpi <aarpi@itba.edu.ar>
2017-07-29 13:10:42 +0000

    Add validations, associations, and sec_password to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  #Associations
+  has_many :todos, foreign_key: :created_by
+  
+  #Validations
+  validates_presence_of :name, :email, :password_digest
+  
+  has_secure_password
 end
