15ddd93d59384e603817bec022faf9990bd5c0ce
Andres Arpi <aarpi@itba.edu.ar>
2017-08-02 20:59:23 +0000

    Update the authenticate user service

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -21,3 +21,29 @@ class AuthenticateUser
     raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
   end
 end
+
+#My version. Does the same thing, the one above is refractored, the one below
+#shows the details of the implementation.
+=begin
+class AuthenticateUser
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+  
+  def call 
+    @user = User.find_by(email: email)
+    if (@user && @user.authenticate(password))
+      return JsonWebToken.encode(user_id: @user.id)
+    else
+      raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
+    end
+  end
+  
+  private
+  attr_reader :email, :password
+end
+=end
+
+
+
