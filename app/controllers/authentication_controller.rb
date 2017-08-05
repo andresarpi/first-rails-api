a4b897ad710c79178e5dde5edb869b48ea69d50c
Andres Arpi <aarpi@itba.edu.ar>
2017-08-05 17:15:45 +0000

    Define the authenticate action for the auhentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,10 @@
 class AuthenticationController < ApplicationController
+  def authenticate
+    auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token)
+  end
+  
+  def auth_params
+    params.permit(:email, :password)
+  end
 end
